f4c3d6d5abd0d64c93d439b86f10901f26531859
Luis Henrique <gavirate.henrique@gmail.com>
2021-07-04 21:00:01 -0300

    feat: modeling entities and validations

---
+++ ManagerAPI/src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using FluentValidation;
+using Manager.Domain.Validators;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        //Propriedades
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        //EF
+        protected User(){}
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        //Comportamentos
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        //Auto-validação
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if(validation.IsValid)
+            {
+                foreach(var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inváldos, por favor corrija-os" + _errors[0]);
+            }
+            return true;
+        }
+    }
+}
