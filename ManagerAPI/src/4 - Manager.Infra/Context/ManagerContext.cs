10afd16ab2e7bae7abd2abed4623fa2b02566420
Luis Henrique <gavirate.henrique@gmail.com>
2021-07-04 22:32:20 -0300

    feat: initial infrastructure implementation

---
+++ ManagerAPI/src/4 - Manager.Infra/Context/ManagerContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Infra.Context
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext()
+        {
+            
+        }
+        
+    }
+}
---
+++ ManagerAPI/src/4 - Manager.Infra/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Manager.Domain.Entities;
+
+namespace Manager.Infra.Interfaces
+{
+    public interface IBaseRepository<T> where T : Base
+    {
+         Task<T> Create(T obj);
+         Task<T> Update(T obj);
+         Task Remove(long id);
+         Task<T> Get(long id);
+         Task<List<T>> Get();
+    }
+}
